3f4af68e349e06929d7e85e9cfa09fbc51f307a9
Mohammad Yazdani <93033366+mohammadyazdani1@users.noreply.github.com>
2023-06-03 17:12:08 +0330

    Add files via upload

---
+++ Darj-contact.sql
@@ -0,0 +1,5 @@
+INSERT INTO contactus(FirstName,LastName,MobileNumber,Email,Title,Message) 
+VALUES ('mohammad','yazdani','09902418285','mohammadyazdani1@gmail.com','Alert','New Message',)
+
+DELETE FROM contactus 
+WHERE Id=1
