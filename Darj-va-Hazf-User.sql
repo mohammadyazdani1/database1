7f4a4b1d63a1ced35a6932a0026b0fac378b953b
Mohammad Yazdani <93033366+mohammadyazdani1@users.noreply.github.com>
2023-06-03 17:06:49 +0330

    Add files via upload

---
+++ Darj-va-Hazf-User.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (firstName,lastName,Username,Password,Email,Status)
+VALUES ('mohammad','yazdani','Moya','12341234','mohammadyazdani1@gamil.com','active');
+
+DELETE FROM users
+WHERE Id=1
+
+
+
+
+
+
